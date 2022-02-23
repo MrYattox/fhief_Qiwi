29c817a74fbdeefa16680d67b13a299fcd3b43a3
MrYattox <98699184+MrYattox@users.noreply.github.com>
2022-02-24 09:13:57 +1000

    Create Balance_check.py

---
+++ Balance_check.py
@@ -0,0 +1,15 @@
+from SimpleQIWI import *
+
+print(‘Q I W I Software 1.0 / coded by m1wka!’);
+
+token=input(‘Enter token: ‘)
+
+phone=input(‘Enter phone: ‘);
+
+api = QApi(token=token, phone=phone)
+
+print(‘Balance Founded’)
+
+print(api.balance)
+
+input()
